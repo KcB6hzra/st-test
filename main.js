9fdf847120982b6b533dd8706e8d815473429e4d
KcB6hzra <176685668+KcB6hzra@users.noreply.github.com>
2024-07-26 23:55:42 +0900

    a

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
+import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest';
 
 (async () => {
   const pat = localStorage.pat;
@@ -14,13 +14,19 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
     return Array.from(new Uint8Array(digest)).map(v => v.toString(16).padStart(2,'0')).join('');
   }
 
+  async function base64encode(text) {
+    const bytes = new TextEncoder().encode(text);
+    const binaryString = Array.from(bytes, byte => String.fromCodePoint(byte)).join('');
+    return btoa(binaryString);
+  }
+
   async function save(octokit, path, content) {
     const OWNER = 'KcB6hzra';
     const REPO = 'test-storage';
     const COMMITTER_NAME = 'KcB6hzra Website';
     const COMMITTER_EMAIL = '176685668+KcB6hzra@users.noreply.github.com';
 
-    const contentEncoded = Base64.encode(content);
+    const contentEncoded = base64encode(content);
 
     const file = await octokit.rest.repos.getContent({
       owner: OWNER,
